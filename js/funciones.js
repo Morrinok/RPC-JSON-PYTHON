b2f5a33efaf0679817b46aefea6164fe793b610d
Sutams <48838483+Sutams@users.noreply.github.com>
2021-11-24 09:41:20 -0300

    Cliente pagina agregado

--- js/funciones.js
+++ js/funciones.js
@@ -116,3 +116,26 @@ function removeItemCarrito (e){
     tr.remove()
     CarritoTotal()
 }
+
+function realizarOrden (e){
+
+    console.log(JSON.stringify(carrito))
+
+    'use strict';
+
+    let assert = require('assert');
+    let pythonBridge = require('python-bridge');
+
+    let python = pythonBridge();
+
+    python.ex`import math`;
+    python`math.sqrt(9)`.then(x => assert.equal(x, 3));
+
+    let list = [3, 4, 2, 1];
+    python`sorted(${list})`.then(x => assert.deepEqual(x, list.sort()));
+
+    python.end();
+
+
+}
+
